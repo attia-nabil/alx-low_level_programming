1748240997b21c56a07d4cc824819ce6072ca7ea
attia-nabil <attia.nabil@hotmail.com>
2023-02-21 14:15:24 +0100

    push

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,18 @@
 #include "main.h"
-  /**
-  * _isalpha - checks for alohabetic character 
-  * @c: character to be checked 
-  * 
-  * Return:1 if character is a letter, 0 otherwise
-  */
-  int _isalpha(int c)
-  {   
-           if((c >= 'a' && c <= 'z') ||
-              (c >= 'A' && c <= 'Z'))
-                      Return (1);
-                 else
-                     Return (0);
-   } 
+/**
+ * _isalpha - check if character is lowercase or uppercase
+ * @c: character to check if it islowercase.
+ * Return: 1 if lowercase/uppercase, 0 if not.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
